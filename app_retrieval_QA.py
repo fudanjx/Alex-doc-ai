2a22135c67dcc52e94ab61c00a83add946048bf6
Tammy29 <97744990+Tammy29@users.noreply.github.com>
2023-08-07 14:03:46 +0800

    Update app_retrieval_QA.py
    Added two functions:
    (1) retrieve_conversation_memory()
    (2) upload_documents()

--- app_retrieval_QA.py
+++ app_retrieval_QA.py
@@ -2,7 +2,9 @@ import os
 import pandas as pd
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.memory import ConversationBufferMemory
 import streamlit as st
+import common_functions as cf
 
 def retrieve_fin_hr_pcm_index():
     embedding=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
@@ -23,3 +25,25 @@ def display_reference(reference_docs):
             reference_str += f"\n\nReference Doc {doc_count+1}: {doc}"
         st.write(reference_str)
     return reference_str
+
+def retrieve_conversation_memory():
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', input_key="context_01", return_messages=True, output_key='answer'
+        )
+    return memory
+
+def upload_documents():
+    # Upload Patient Notes
+    context=""
+    uploaded_files = st.file_uploader("Upload relevant documents for context", 
+                                      type=["pdf", "xlsx", 'csv', 'xls', 'docx'], accept_multiple_files=True)
+    if uploaded_files:
+        count=0
+        for uploaded_file in uploaded_files:
+            count+=1
+            if uploaded_file.name.endswith(('.pdf')):
+                context += cf.retrieve_multi_pdf_text(upload_file)
+            elif uploaded_file.name.endswith(('.xlsx', '.xls', '.csv')):
+                extracted_text = cf.retrieve_multi_excel_text(upload_file)
+                context += f"Document {count}:{extracted_text}"
+    return context
